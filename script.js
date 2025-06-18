1b6cc8f0147468c44ca5ceeeea552a38df9591f7
J0j0-4951 <jhos3lin3s.z@gmail.com>
2025-06-18 17:49:15 +0000

    added function

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-{
+
 const header = document.querySelector("h1");
 console.log(header);
-const blue = document.queryzselectorAll("blueclass");
+const btn = getElementById("Submit!");
+
+const textbox = document.getElementById("nameinput");
+const header = DocumentFragment.getElementById("h1");
+let name ="";
+btn.addEventListener("click", () =>{
+    event.preventDefault();
+    name = textbox.value;
+header.textContent = "Welcoome, " + name + "!";
+
+const blue = document.querySelectorAll("blueclass");
 console.log(blue);
-document.getElementById("Submit!").addzeventListener("Submit!",function()
-}
-{
-alert("Welcome, Name!");
-}
+});
