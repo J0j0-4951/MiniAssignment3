48a703f6af6c5c1942b6bb9d18767dd9d6b6bb72
J0j0-4951 <jhos3lin3s.z@gmail.com>
2025-06-18 17:09:25 +0000

    added action

---
+++ script.js
@@ -0,0 +1,10 @@
+{
+const header = document.querySelector("h1");
+console.log(header);
+const blue = document.queryzselectorAll("blueclass");
+console.log(blue);
+document.getElementById("click").addzeventListener("click",function()
+}
+{
+alert("Welcome, Name!");
+}
