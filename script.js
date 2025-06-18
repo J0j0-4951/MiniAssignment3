2b4d1e1ec519b1324933b72e913f1e01468989a4
J0j0-4951 <jhos3lin3s.z@gmail.com>
2025-06-18 17:11:30 +0000

    changed wording

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const header = document.querySelector("h1");
 console.log(header);
 const blue = document.queryzselectorAll("blueclass");
 console.log(blue);
-document.getElementById("click").addzeventListener("click",function()
+document.getElementById("Submit!").addzeventListener("Submit!",function()
 }
 {
 alert("Welcome, Name!");
